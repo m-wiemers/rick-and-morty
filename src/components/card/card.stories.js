f23bb91b43435599610cc1155f184a2ee69a61e0
m-wiemers <marcel@klebauter.de>
2021-02-08 17:14:01 +0100

    add cards

---
+++ src/components/card/card.stories.js
@@ -0,0 +1,25 @@
+import "./card.css";
+import { createCard } from "./card";
+
+export default {
+  title: "Components/Cards",
+  parameters: { layout: "centered" },
+};
+
+export const Rick = () =>
+  createCard({
+    imgSrc: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+    name: "Rick Sanches",
+    status: "Alive",
+    species: "Human",
+    origin: "Earth (C-137)",
+  });
+
+export const Morty = () =>
+  createCard({
+    imgSrc: "https://rickandmortyapi.com/api/character/avatar/2.jpeg",
+    name: "Morty Smith",
+    status: "Alive",
+    species: "Human",
+    origin: "Earth (C-137)",
+  });
