ff5fd7f5e319d84512f36d9c1633222a474b23de
m-wiemers <marcel@klebauter.de>
2021-02-10 17:02:54 +0100

    add filter for name

--- src/components/card/card.stories.ts
+++ src/components/card/card.stories.ts
@@ -107,3 +107,35 @@ export const RandomCharacter = () => {
   });
   return container;
 };
+
+export const CharactersFromAPIwithFilter = (
+  args,
+  { loaded: { characters } }
+) => {
+  const input = createElement("input", {
+    onchange: async () => {
+      const newCharacters = await getCharacters(input.value);
+      const newCards = newCharacters.map((character) => createCard(character));
+      characterContainer.innerHTML = "";
+      characterContainer.append(...newCards);
+    },
+  });
+
+  const characterContainer = createElement("div", {
+    className: "container",
+    childs: characters.map((character) => createCard(character)),
+  });
+
+  const container = createElement("div", {
+    className: "",
+    childs: [input, characterContainer],
+  });
+
+  return container;
+};
+
+CharactersFromAPIwithFilter.loaders = [
+  async () => ({
+    characters: await getCharacters(),
+  }),
+];
