0cea9bc893d815b9de6ab32590a0afc56add8141
m-wiemers <marcel@klebauter.de>
2021-02-11 12:10:49 +0100

    add HTML flip-card elements in createCard

--- src/components/card/card.ts
+++ src/components/card/card.ts
@@ -4,29 +4,49 @@ export function createCard({ imgSrc, name, status, species, origin }) {
   return createElement("article", {
     className: "card",
     childs: [
-      createElement("img", {
-        className: "card__portrait",
-        src: imgSrc,
-        alt: "",
-      }),
       createElement("div", {
-        className: "card__info",
+        className: "card__inner",
         childs: [
-          createElement("h2", {
-            className: "card__name",
-            innerText: name,
+          createElement("div", {
+            className: "card__front",
+            childs: [
+              createElement("img", {
+                className: "card__portrait",
+                src: imgSrc,
+                alt: "",
+              }),
+              createElement("div", {
+                className: "card__info",
+                childs: [
+                  createElement("h2", {
+                    className: "card__name",
+                    innerText: name,
+                  }),
+                  createElement("p", {
+                    className: "card__status",
+                    innerText: `${
+                      status === "Alive" ? "🥳🎉" : "💀"
+                    } - ${status}`,
+                  }),
+                  createElement("p", {
+                    className: "card_species",
+                    innerText: "Species: " + species,
+                  }),
+                  createElement("p", {
+                    className: "card__origin",
+                    innerText: "Origin: " + origin.name,
+                  }),
+                ],
+              }),
+            ],
           }),
-          createElement("p", {
-            className: "card__status",
-            innerText: `${status === "Alive" ? "🥳🎉" : "💀"} - ${status}`,
-          }),
-          createElement("p", {
-            className: "card_species",
-            innerText: "Species: " + species,
-          }),
-          createElement("p", {
-            className: "card__origin",
-            innerText: "Origin: " + origin.name,
+          createElement("div", {
+            className: "card__back",
+            childs: [
+              createElement("p", {
+                innerText: "kommt noch",
+              }),
+            ],
           }),
         ],
       }),
